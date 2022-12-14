c6d617275595076921cca23b173d0359f351d26b
MoreiraSte <casagrandestephany91@gmail.com>
2022-12-14 15:47:01 -0300

    making navbar

---
+++ src/components/home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import NavBar from '../components/navBar';
+
+const Hero = () => {
+    return(
+        <>       
+         <NavBar/>
+        <div className="Hero">
+            <h1>Helloooo</h1>
+
+        </div>
+        </>
+
+    )
+}
+
+export default Hero;
---
+++ src/components/navBar.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+// import { Link } from 'react-router-dom';
+// import {Button} from '../components/Button'
+import '../styles/navBar.css'
+
+const NavBar = () => {
+
+    <><nav className='navbar'>
+    <div className="navbarContainer">
+      wetwgw
+
+    </div>
+    
+   
+  </nav>
+  </>
+  }
+
+  export default NavBar
